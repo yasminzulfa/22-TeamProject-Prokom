8d189203890c651c1dffbf6500a26c10977a6b22
Yunita <yunita3640@gmail.com>
2022-06-10 17:14:26 +0700

    menambah modul 2

--- Team Project/Modul.py
+++ Team Project/Modul.py
@@ -9,3 +9,16 @@ def cekKota():
         break
       else:
         print("Kota tujuan tidak terdaftar, silahkan masukkan kembali")
+
+def rekomendasi(ekspedisi, harga2):
+    
+    tampilan = {'Ekpedisi' : ekspedisi,
+                 'Harga': harga2}
+    df_tampil = pd.DataFrame(tampilan, index = np.arange(1, 6))
+    dft = df_tampil.sort_values(by = 'Harga')
+    dft.index = np.arange(1,6)
+    return(dft)
+
+def hitungongkir(kota, berat, ekspedisi):
+    harga_ongkir = (Harga.loc[df['ibukota'] == kota, ekspedisi].iloc[0])*berat
+    return harga_ongkir
