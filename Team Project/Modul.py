fad3f4107e62c180771286037ce31ccea88edb1c
WhisnuPY <101338035+WhisnuPY@users.noreply.github.com>
2022-06-12 01:49:24 +0700

    Menambah Fitur

--- Team Project/Modul.py
+++ Team Project/Modul.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_excel("Database.xlsx")
+df = pd.read_excel('Database.xlsx')
+df2 = pd.read_excel('Daftar Pickup.xlsx')
 df_baru = df.dropna()
-Harga = df_baru.drop('no.', axis =1)
+Harga = df_baru.drop('kode', axis =1)
+Harga2 = df2.drop('No', axis =1)
 
 def cekKota():
     while True:
-      tujuan = input("Masukkan kota tujuan: ")
+      tujuan = input("Masukkan kode kota tujuan (Contoh : Jakarta) : ")
       tujuan = tujuan.lower()
       cek = tujuan in df.ibukota.unique()
       if cek == True:
@@ -16,6 +18,18 @@ def cekKota():
         break
       else:
         print("Kota tujuan tidak terdaftar, silahkan masukkan kembali")
+        
+def cekKotaAsal():
+    while True:
+      pickup = input("Masukkan kota Pickup (Contoh : Surakarta) : ")
+      pickup = pickup.capitalize()
+      cek = pickup in df2.tempat.unique()
+      if cek == True:
+        print("Kota pickup Anda: " + pickup)
+        return pickup
+        break
+      else:
+        print("Kota pickup tidak terdaftar, silahkan masukkan kembali")
 
 def rekomendasi(ekspedisi, harga2):
     
@@ -26,6 +40,8 @@ def rekomendasi(ekspedisi, harga2):
     dft.index = np.arange(1,6)
     return(dft)
 
-def hitungongkir(kota, berat, ekspedisi):
+def hitungongkir(kota, berat, ekspedisi, fee):
     harga_ongkir = (Harga.loc[df['ibukota'] == kota, ekspedisi].iloc[0])*berat
-    return harga_ongkir
+    fee = (Harga2.loc[df2['tempat'] == fee, ekspedisi].iloc[0]) * berat
+    total = harga_ongkir + fee
+    return total
--- Team Project/Projek_Pemrograman.py
+++ Team Project/Projek_Pemrograman.py
@@ -1,47 +1,59 @@
 import pandas as pd
-import numpy as np
 import Modul as m
 
-df = pd.read_excel("Database.xlsx")
+df = pd.read_excel('Database.xlsx')
+df2 = pd.read_excel('Daftar Pickup.xlsx')
 df_baru = df.dropna()
+Harga = df_baru.drop('kode', axis =1)
+Harga2 = df2.drop('No', axis =1)
 
-print('Selamat Datang di Ongkirku')
-print("Berikut adalah daftar kota yang ada dalam program kami:")
-for index, row in df_baru.iterrows():
-    print(str(index) + ".", row["ibukota"].title())
-print()
+print('Selamat Datang di Ongkirku ^_^')
 while True:
-      cari = input('Apakah kamu mau mencari biaya ongkir dari Jawa Tengah?(iya/tidak): ')
+    cari = input('Apakah kamu mau mencari biaya ongkir dari Jawa Tengah?(iya/tidak): ')
+    print()
+
       
 
-      if cari.lower() == 'iya': 
-          tujuan = m.cekKota()
+    if cari.lower() == 'iya': 
+        print("Berikut adalah lokasi pickup yang ada dalam program kami:")
+        
 
-          while True:
+        for index, row in df2.iterrows():
+            print(str(index + 1) + ".", row["tempat"].title())
+        kota_asal = m.cekKotaAsal()
+        print()
+
+        print("Berikut adalah daftar kota yang ada dalam program kami:")
+        for index, row in df_baru.iterrows():
+            print(str(index) + ".", row["ibukota"].title())
+        tujuan = m.cekKota()
+        print()
+        while True:
                 
                 #bila ingin masukkan berat bisa float
-                try :
-                    berat = float(input('Masukkan berat paket(kg): '))
-                    print('berat paket Anda:', berat ,'Kg')
-                    break
-                except:
-                    print('Masukkan data berupa angka')
+            try :
+                berat = float(input('Masukkan berat paket(kg): '))
+                print('berat paket Anda:', berat ,'Kg')
+                break
+            except:
+                print('Masukkan data berupa angka')
             
-          ekspedisi = ['J&T Express', 'JNE', 'SiCepat Express','Tiki ','Pos Indonesia']
-          harga2 = []
+        ekspedisi = ['J&T Express', 'JNE', 'SiCepat Express','Tiki ','Pos Indonesia']
+        harga2 = []
 
-          for i in ekspedisi:
-              ber = m.hitungongkir(tujuan,berat, i)
-              harga2.append(int(ber))
+        for i in ekspedisi:
+            ber = m.hitungongkir(tujuan,berat, i, kota_asal)
+            harga2.append(int(ber))
+            
 
-          print()
-          print('Ini list rekomendasi kami untuk mu ^_^')
-          print(m.rekomendasi(ekspedisi, harga2))
-          print()
+        print()
+        print('Ini list rekomendasi kami untuk mu ^_^')
+        print(m.rekomendasi(ekspedisi, harga2))
+        print()
       
-      elif cari.lower() == 'tidak':
-          print('Oke, Terima Kasih telah menggunakan Ongkirku ^_^')
-          break
+    elif cari.lower() == 'tidak':
+        print('Oke, Terima Kasih telah menggunakan Ongkirku ^_^')
+        break
 
-      else:
-          print('Silahkan pilih iya atau tidak')
+    else:
+        print('Silahkan pilih iya atau tidak')
