3d981de012d6e08502f5da0a7359f39b20989530
WhisnuPY <101338035+WhisnuPY@users.noreply.github.com>
2022-06-17 02:08:17 +0700

    Merapikan string dan direktori files

--- Modul.py
+++ Modul.py
@@ -7,30 +7,41 @@ df_baru = df.dropna()
 Harga = df_baru.drop('kode', axis =1)
 Harga2 = df2.drop('No', axis =1)
 
+#Yunita
 def cekKota():
     while True:
-      tujuan = input("Masukkan kode kota tujuan (Contoh : Jakarta) : ")
+      tujuan = input("Masukkan kode kota tujuan (Contoh : Jakarta)\t: ")
       tujuan = tujuan.lower()
-      cek = tujuan in df.ibukota.unique()
+      cek = tujuan in df.kota.unique()
       if cek == True:
-        print("Kota tujuan Anda: " + tujuan.capitalize())
+        print("Kota tujuan Anda\t\t\t\t: " + tujuan.capitalize())
         return tujuan
         break
       else:
         print("Kota tujuan tidak terdaftar, silahkan masukkan kembali")
-        
+
+#Kiki        
 def cekKotaAsal():
     while True:
-      pickup = input("Masukkan kota Pickup (Contoh : Surakarta) : ")
+      pickup = input("Masukkan kota Pickup (Contoh : Surakarta): ")
       pickup = pickup.capitalize()
       cek = pickup in df2.tempat.unique()
       if cek == True:
-        print("Kota pickup Anda: " + pickup)
+        print("Kota pickup Anda  \t\t\t : " + pickup)
         return pickup
         break
       else:
         print("Kota pickup tidak terdaftar, silahkan masukkan kembali")
 
+#Yasmin
+def hitungongkir(kota, berat, ekspedisi, fee):
+    harga_ongkir = (Harga.loc[df['kota'] == kota, ekspedisi].iloc[0])*berat
+    fee = (Harga2.loc[df2['tempat'] == fee, ekspedisi].iloc[0]) * berat
+    total = harga_ongkir + fee
+    return total
+
+
+#Yasmin
 def rekomendasi(ekspedisi, harga2):
     
     tampilan = {'Ekpedisi' : ekspedisi,
@@ -39,9 +50,3 @@ def rekomendasi(ekspedisi, harga2):
     dft = df_tampil.sort_values(by = 'Harga')
     dft.index = np.arange(1,6)
     return(dft)
-
-def hitungongkir(kota, berat, ekspedisi, fee):
-    harga_ongkir = (Harga.loc[df['ibukota'] == kota, ekspedisi].iloc[0])*berat
-    fee = (Harga2.loc[df2['tempat'] == fee, ekspedisi].iloc[0]) * berat
-    total = harga_ongkir + fee
-    return total
--- Projek_Pemrograman.py
+++ Projek_Pemrograman.py
@@ -1,39 +1,45 @@
 import pandas as pd
 import Modul as m
+import numpy as np
 
 df = pd.read_excel('Database.xlsx')
 df2 = pd.read_excel('Daftar Pickup.xlsx')
 df_baru = df.dropna()
+df_kota_asal = df2.sort_values(by= "tempat").reset_index(drop=True)
+df_kota_tujuan = df_baru.sort_values(by= "kota").reset_index(drop=True)
 Harga = df_baru.drop('kode', axis =1)
 Harga2 = df2.drop('No', axis =1)
 
-print('Selamat Datang di Ongkirku ^_^')
+print('======================================================================')
+print()
+print('Selamat Datang di Ongkirku ^_^'.center(60))
 while True:
-    cari = input('Apakah kamu mau mencari biaya ongkir dari Jawa Tengah?(iya/tidak): ')
+    cari = input('Apakah kamu mau mencari biaya ongkir dari Jawa Tengah?(iya/tidak): '.center(55))
     print()
-
+    print('======================================================================')
       
 
     if cari.lower() == 'iya': 
         print("Berikut adalah lokasi pickup yang ada dalam program kami:")
         
 
-        for index, row in df2.iterrows():
+        for index, row in df_kota_asal.iterrows():
             print(str(index + 1) + ".", row["tempat"].title())
+        print()
         kota_asal = m.cekKotaAsal()
         print()
-
-        print("Berikut adalah daftar kota yang ada dalam program kami:")
-        for index, row in df_baru.iterrows():
-            print(str(index) + ".", row["ibukota"].title())
-        tujuan = m.cekKota()
+        print('======================================================================')
         print()
-        while True:
-                
+        print("Berikut adalah daftar kota yang ada dalam program kami:")
+        for index, row in df_kota_tujuan.iterrows():
+            print(str(index) + ".", row["kota"].title())
+        print()
+        tujuan = m.cekKota()
+        while True:   
                 #bila ingin masukkan berat bisa float
             try :
-                berat = float(input('Masukkan berat paket(kg): '))
-                print('berat paket Anda:', berat ,'Kg')
+                berat = float(input('Masukkan berat paket(kg)\t\t\t: '))
+                print('Berat paket Anda\t\t\t\t: {} Kg'.format(berat))
                 break
             except:
                 print('Masukkan data berupa angka')
@@ -47,12 +53,16 @@ while True:
             
 
         print()
+        print('======================================================================')
+        print()
         print('Ini list rekomendasi kami untuk mu ^_^')
+        print()
         print(m.rekomendasi(ekspedisi, harga2))
         print()
       
     elif cari.lower() == 'tidak':
-        print('Oke, Terima Kasih telah menggunakan Ongkirku ^_^')
+        print('Oke, Terima Kasih telah menggunakan Ongkirku ^_^'.center(67))
+        print()
         break
 
     else:
